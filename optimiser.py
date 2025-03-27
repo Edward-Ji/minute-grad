58d2b25388d11555fd04e75a4026d959e6266d5a
Edward Ji <jiziao6@gmail.com>
2025-03-27 15:43:15 +1100

    Add Adam optimiser

--- optimiser.py
+++ optimiser.py
@@ -10,6 +10,7 @@ class Optimiser:
         for tensor in self.tensors:
             tensor.grad = np.zeros_like(tensor.grad)
 
+
 class GradientDescentOptimiser(Optimiser):
 
     def __init__(self, tensors, learning_rate=1e-3):
@@ -23,26 +24,30 @@ class GradientDescentOptimiser(Optimiser):
 
 class AdamOptimiser(Optimiser):
 
-    def __init__(self,
-                 tensors,
-                 learning_rate=0.001,
-                 beta1=0.9,
-                 beta2=0.999,
-                 epsilon=1e-8):
+    def __init__(
+        self, tensors, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8
+    ):
         super().__init__(tensors)
         self.learning_rate = learning_rate
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
-        self.iteration = 1
-        self.tensors = list(tensors)
+        self.first_moment = {tensor: np.zeros_like(tensor.grad)
+                             for tensor in self.tensors}
+        self.second_moment = {tensor: np.zeros_like(tensor.grad)
+                              for tensor in self.tensors}
 
-    def zero_grad(self)
-
-    def optimise(self, tensor):
-        # update the first moment
-        tensor.first_moment = ((self.beta1 * tensor.first_moment) + ((1 - self.beta1) * tensor.grad))/(1 - (self.beta1 ** self.iteration))
-        # update the second moment
-        tensor.second_moment = (self.beta2 * tensor.second_moment) + ((1 - self.beta2) * np.square(tensor.grad))/(1 - (self.beta2 ** self.iteration))
-        # update the parameters
-        tensor.data = tensor.data - ((self.learning_rate/(np.sqrt(tensor.second_moment) + self.epsilon)) * self.first_moment)
+    def optimise(self):
+        for tensor in self.tensors:
+            self.first_moment[tensor] = (
+                self.beta1 * self.first_moment[tensor] + (1 - self.beta1) * tensor.grad
+            )
+            self.second_moment[tensor] = (
+                self.beta2 * self.second_moment[tensor]
+                + (1 - self.beta2) * tensor.grad**2
+            )
+            tensor.val -= (
+                self.learning_rate
+                * self.first_moment[tensor]
+                / (np.sqrt(self.second_moment[tensor]) + self.epsilon)
+            )
