fixing softmax

blobs_demo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.datasets import make_blobs
 
-from layer import Composite, CrossEntropyLoss, Dropout, Linear, ReLU
+from layer import Composite, CrossEntropyLoss, Dropout, Linear, ReLU, Softmax
 from optimiser import AdamOptimiser
 from tensor import Tensor
 from util import BatchGenerator, train_test_split, xavier_uniform
@@ -20,18 +20,15 @@ def main():
     X_test = Tensor(X_test)
     y_test = Tensor(y_test)
 
-    batches_trian = BatchGenerator(X_train, y_train, batch_size=32)
+    batches_train = BatchGenerator(X_train, y_train, batch_size=32)
 
     # linear model
     model = Composite(
         [
             Linear(2, 32),
             ReLU(),
-            Dropout(0.1),
-            Linear(32, 32),
-            ReLU(),
-            Dropout(0.1),
-            Linear(32, 3, initialise=xavier_uniform)
+            Linear(32, 3, initialise=xavier_uniform),
+            Softmax(3, 3)
         ]
     )
     optimiser = AdamOptimiser(model.get_all_tensors())
@@ -42,9 +39,8 @@ def main():
     accuracy = np.mean(np.argmax(logits.val, axis=1) == y_test.val)
     print(f"Before training test accuracy: {accuracy:.2f}")
     for _ in range(100):
-
-    model.train(True)
-        for X_batch, y_batch in batches_trian:
+        model.train(True)
+        for X_batch, y_batch in batches_train:
             optimiser.zero_grad()
             logits = model(X_batch)
             loss = loss_fn(logits, y_batch)

layer.py
@@ -108,14 +108,26 @@ class Sigmoid(Layer):
 
 class Softmax(Layer):
 
+    def __init__(self, features, num_outputs, initialise=kaiming_uniform):
+        self.weights = Tensor(initialise(features, num_outputs), True)
+        self.bias = Tensor(np.zeros((1, num_outputs)), True)
+
     def forward(self, x) -> Tensor:
         # Note: calculating this may cause issues if the values in x get fairly large (even up to 1000 can cause issues)
         # May not be an issue for now, but might consider doing some normalisation to ensure consistency
         # See https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative for more details
-        out = Tensor(np.exp(x.val) / np.sum(np.exp(x.val)), x.requires_grad)
+        # TODO: might be the wrong way around
+        weighted = x @ self.weights + self.bias
 
+        out = Tensor(np.exp(x.val) / np.sum(np.exp(x.val), axis=1).reshape(len(x.val), 1), x.requires_grad)
         def _backward():
-            x.grad += unbroadcast((np.diag(out.val) - np.outer(out.val, out.val)), x.shape)
+            diag = np.array([np.diag(out.val[i]) for i in range(len(out.val))])
+            outer = np.array([np.outer(out.val[i], out.val[i]) for i in range(len(out.val))])
+            jacobian_gradient = np.array([diag[i] - outer[i] for i in range(len(diag))])
+            for i in range(len(x.val)):
+                self.weights.grad += jacobian_gradient[i] * x.val[i]
+                self.bias.grad += np.sum(jacobian_gradient[i], axis=1)
+            x.grad += unbroadcast(x.grad @ self.weights.val, x.shape)
 
         out._backward = _backward
         out._prev = {x}