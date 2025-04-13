fixed batch normalisation

experiments/all_experiments.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -299,12 +300,12 @@ batchnorm_labels = ["With Batch Normalisation", "Without Batch Normalisation"]
 
 
 experiments = [
-    [depth_models, depth_labels],
+    [depth_models, depth_labels]
     [width_models, width_labels],
     [dropout_models, dropout_labels],
     [dropout_3l_models, dropout_3l_labels],
     [activation_models, activation_labels],
-    [batchnorm_models, batchnorm_labels],
+    [batchnorm_models, batchnorm_labels]
 ]
 
 folders = [
@@ -324,19 +325,22 @@ def run_experiment(folder, models, model_labels):
     all_test_loss = []
     all_test_accuracy = []
 
+    training_times = []
+    inference_times = []
+
     for model in models:
         X_train = Tensor(standard_scale(np.load("../data/train_data.npy")))
         y_train = Tensor(np.load("../data/train_label.npy").squeeze())
         X_test = Tensor(standard_scale(np.load("../data/test_data.npy")))
         y_test = Tensor(np.load("../data/test_label.npy").squeeze())
 
-        epochs = 50
+        epochs = 100
         batch_size = 64
 
         optimiser = AdamOptimiser(model.get_all_tensors(), weight_decay=1e-5)
         loss_fn = CrossEntropyLoss(label_smoothing=0.3)
 
-        train_loss_lst, train_acc_lst, test_loss, test_accuracy = train_loop(
+        train_loss_lst, train_acc_lst, test_loss, test_accuracy, training_time, inference_time = train_loop(
             X_train,
             y_train,
             X_test,
@@ -347,6 +351,9 @@ def run_experiment(folder, models, model_labels):
             optimiser,
             loss_fn,
         )
+
+        training_times.append(training_time)
+        inference_times.append(inference_time)
 
         all_training_loss_lst.append(train_loss_lst)
         all_train_acc_lst.append(train_acc_lst)
@@ -364,6 +371,8 @@ def run_experiment(folder, models, model_labels):
         [x[-1] for x in all_train_acc_lst],
         all_test_loss,
         all_test_accuracy,
+        training_times,
+        inference_times
     )
 
 

experiments/train_util.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import os
+import time
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
@@ -15,15 +16,19 @@ from util import BatchGenerator
 def train_loop(
     X_train, y_train, X_test, y_test, epochs, batch_size, model, optimiser, loss_fn
 ):
+    # Define a training loop to be used by a model, which conducts both training and testing of the model
     batches = BatchGenerator(X_train, y_train, batch_size=batch_size)
 
     train_loss_lst = []
     train_acc_lst = []
+    start_time = time.time()
 
+    # Training loop
     for epoch in tqdm(range(epochs)):
         train_loss = 0
         train_accuracy = 0
         model.train(True)
+        # For each batch, produce a prediction and calculate the loss
         for X_batch, y_batch in batches:
             optimiser.zero_grad()
             logits = model(X_batch)
@@ -37,28 +42,37 @@ def train_loop(
         if optimiser.name == 'Adam':
             optimiser.iterations += 1
         
+        # Calculate the training loss and accuracy
+
         train_loss /= len(batches)
         train_accuracy /= len(batches)
 
         train_loss_lst.append(train_loss)
         train_acc_lst.append(train_accuracy)
 
-        if epoch % 10 == 0:
-            model.train(False)
-            test_logits = model(X_test)
-            test_loss = loss_fn(test_logits, y_test).val.item()
-            test_accuracy = np.mean(np.argmax(test_logits.val, axis=1) == y_test.val)
-            tqdm.write(
-                f"Epoch {epoch}, "
-                f"Train Loss: {train_loss:.4f}, "
-                f"Train Accuracy: {train_accuracy:.2%}, "
-                f"Test Loss: {test_loss:.4f},"
-                f"Test Accuracy: {test_accuracy:.2%}"
-            )
+    # Note the time taken to train
+    training_time = time.time() - start_time
+
+    # After training is completed, test the model to see how well it performs
+    start_time = time.time()
+    model.train(False)
+    test_logits = model(X_test)
+    inference_time = time.time() - start_time
+    test_loss = loss_fn(test_logits, y_test).val.item()
+    test_accuracy = np.mean(np.argmax(test_logits.val, axis=1) == y_test.val)
+    tqdm.write(
+        f"Epoch {epoch}, "
+        f"Train Loss: {train_loss:.4f}, "
+        f"Train Accuracy: {train_accuracy:.2%}, "
+        f"Test Loss: {test_loss:.4f},"
+        f"Test Accuracy: {test_accuracy:.2%}"
+    )
+
+    # Reset the optimiser if necessary
     if optimiser.name == 'Adam':
         optimiser.iterations = 1
 
-    return train_loss_lst, train_acc_lst, test_loss, test_accuracy
+    return train_loss_lst, train_acc_lst, test_loss, test_accuracy, training_time, inference_time
 
 
 def plot_losses_and_accuracies(
@@ -93,7 +107,7 @@ def plot_losses_and_accuracies(
 
 
 def plot_losses(filename, model_labels, training_losses):
-    # Plotting
+    # Plotting training lossses across the number of epochs trained for each of the different model types
     plt.figure(figsize=(10, 6))
     for i, losses in enumerate(training_losses):
         label = model_labels[i] if i < len(model_labels) else f"Model {i}"
@@ -110,7 +124,8 @@ def plot_losses(filename, model_labels, training_losses):
     plt.close()
 
 
-def save_loss_accuracy(filename, labels, train_loss, train_acc, losses, accuracies):
+def save_loss_accuracy(filename, labels, train_loss, train_acc, losses, accuracies, training_time, inference_time):
+    # Produce a CSV which contains the training and test losses and accuracies, as well as the training and inference times
     with open(f"{filename}.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(
@@ -120,9 +135,11 @@ def save_loss_accuracy(filename, labels, train_loss, train_acc, losses, accuraci
                 "Training Accuracy",
                 "Test Loss",
                 "Test Accuracy",
+                "Training time (seconds)",
+                "Inference time (seconds)"
             ]
         )
-        for label, t_loss, t_acc, loss, acc in zip(
-            labels, train_loss, train_acc, losses, accuracies
+        for label, t_loss, t_acc, loss, acc, train_time, test_time in zip(
+            labels, train_loss, train_acc, losses, accuracies, training_time, inference_time
         ):
-            writer.writerow([label, t_loss, t_acc, loss, acc])
+            writer.writerow([label, t_loss, t_acc, loss, acc, train_time, test_time])

layer.py
@@ -176,11 +176,23 @@ class BatchNormalisation(Layer):
         self.epsilon = epsilon
         self.weights = Tensor(np.ones((1, features)), True)
         self.bias = Tensor(np.zeros((1, features)), True)
+        self.averages = np.zeros((1, features))
+        self.variances = np.zeros((1, features))
+        self.num_examples = 0
 
     def forward(self, x) -> Tensor:
-        mu = np.mean(x.val, axis=0)
-        sigma_squared = np.var(x.val, axis=0)
         batch_size = len(x.val)
+        if self.training:
+            # if we are training, we need to store a running mean and variance of the values
+            mu = np.mean(x.val, axis=0)
+            self.averages = ((self.averages * self.num_examples) + np.sum(x.val, axis=0)) / (self.num_examples + batch_size)
+            sigma_squared = np.var(x.val, axis=0)
+            self.variances = ((self.variances * self.num_examples) + (np.var(x.val, axis=0) * batch_size)) / (self.num_examples + batch_size)
+            self.num_examples += batch_size
+        else:
+            mu = self.averages
+            sigma_squared = self.variances
+        
         normalised_data = (x.val - mu) / np.sqrt(sigma_squared + self.epsilon)
         # We do an elementwise multiplication here with the weights because normalised data is a tensor containing multiple input samples
         # Therefore, we want to multiply the weight array with each respective column in the tensor